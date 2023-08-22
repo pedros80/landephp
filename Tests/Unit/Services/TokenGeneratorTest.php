8cb35683179387242ba382e0d122b4a5af946582
Peter Somerville <peter@geckoengage.com>
2023-08-22 23:02:52 +0100

    more tests

--- Tests/Unit/Services/TokenGeneratorTest.php
+++ Tests/Unit/Services/TokenGeneratorTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Services;
 
 use Exception;
 use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
 use Pedros80\LANDEphp\Exceptions\CouldNotGenerateToken;
 use Pedros80\LANDEphp\Services\TokenGenerator;
 use PHPUnit\Framework\TestCase;
@@ -27,5 +28,22 @@ final class TokenGeneratorTest extends TestCase
         );
 
         $service->getToken();
+    }
+
+    public function testCanGetTokenAndParseIt(): void
+    {
+        $client = $this->prophesize(Client::class);
+
+        $client->post('')->shouldBeCalled()->willReturn(new Response(200, [], '{"user_id":"user_id","expires_in":8900,"access_token":"access_token"}'));
+
+        $service = new TokenGenerator(
+            $client->reveal()
+        );
+
+        $token = $service->getToken();
+
+        $this->assertIsArray($token);
+        $this->assertEquals('user_id', $token['user']);
+        $this->assertEquals('access_token', $token['token']);
     }
 }
